658d4e0c679e95a0b7b7afa78f10b0f2dedf45e8
Mohit Rathore <rathoremohit633@gmail.com>
2024-03-16 23:57:06 +0530

    error 422

--- services/Mailer.js
+++ services/Mailer.js
@@ -8,7 +8,7 @@ class Mailer {
       domain: keys.mailgunDomain,
     });
     this.data = {
-      from: "your_mailgun_email@gmail.com",
+      from: "rathore.mohit6337@gmail.com",
       to: this.formatAddresses(recipients),
       subject: subject,
       html: content,
